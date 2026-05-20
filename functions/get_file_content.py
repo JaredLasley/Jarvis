1e63c0dbfdc2f013b0bf5fb6369c97de8fd81bdb
JaredLasley <lasleyjared@gmail.com>
2026-05-20 19:53:28 -0400

    Corrected try block inclusion in get_file_content

--- functions/get_file_content.py
+++ functions/get_file_content.py
@@ -1,17 +1,18 @@
 import os
 import config
 def get_file_content(working_directory: str, file_path: str) -> str:
-    abs_working_dir:str = os.path.abspath(working_directory)
-    joined:str = os.path.join(abs_working_dir,file_path)
-    target:str = os.path.abspath(joined)
+    try:
+        abs_working_dir:str = os.path.abspath(working_directory)
+        joined:str = os.path.join(abs_working_dir,file_path)
+        target:str = os.path.abspath(joined)
     
 
-    valid_target_dir:bool = os.path.commonpath([abs_working_dir, target]) == abs_working_dir
-    if not valid_target_dir:
-        return f'Error: Cannot read "{file_path}" as it is outside the permitted working directory'
-    if not os.path.isfile(target):
-        return f'Error: File not found or is not a regular file: "{file_path}"'
-    try:
+        valid_target_dir:bool = os.path.commonpath([abs_working_dir, target]) == abs_working_dir
+        if not valid_target_dir:
+            return f'Error: Cannot read "{file_path}" as it is outside the permitted working directory'
+        if not os.path.isfile(target):
+            return f'Error: File not found or is not a regular file: "{file_path}"'
+    
         with open(target,"r") as f:
             
             file_content_string = f.read(config.MAX_CHARS)
