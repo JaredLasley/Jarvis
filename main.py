34f9b3c907c946551d8ae3e87cb29909aeff870f
JaredLasley <lasleyjared@gmail.com>
2026-05-24 22:02:03 -0400

    I: Added the agentic loop in man to iterate over itself

--- main.py
+++ main.py
@@ -22,41 +22,54 @@ def main ():
 		raise RuntimeError("No API key found. Please input api key")
 	#defining the google genai objects (query is the model, and usage is for accessing usage metrics)
 	client = genai.Client(api_key=api_key)
-	query = client.models.generate_content(
-		model="gemini-2.5-flash",contents=messages, 
-		config=types.GenerateContentConfig(
-			tools = [available_functions],
-			system_instruction=system_prompt,
-			temperature = 0))
-	usage = query.usage_metadata
-	#printing all of the requested info if verbose flag is present
-	if args.verbose:
-		print(f"User prompt:{args.user_prompt}")
-		print(f"Prompt tokens: {usage.prompt_token_count}")
-		print(f"Response tokens: {usage.candidates_token_count}\n")
-	#error handling for Failed Token API request
-	if query.usage_metadata is None:
-		raise RuntimeError("Failed API Request: Token counts unavailable")
-
-	if not query.function_calls:
-		print("Response:")
-		print(query.text)
-	else:
-		function_responses = []
-		for call in query.function_calls:
-			function_call_result = call_function(call,args.verbose)
-			if not function_call_result.parts:
-				raise Exception("call_function return object is empty")
-			if not function_call_result.parts[0].function_response:
-				raise Exception("fucntion response list is empty")
-			if not function_call_result.parts[0].function_response.response:
-				raise Exception("functio response attribute is empty ")
 	
-			function_responses.append(function_call_result.parts[0])
-			if args.verbose:
-				print(f"-> {function_call_result.parts[0].function_response.response}")
+	#loop to create the "agentic" functionallity
+	for i in range(20):
+		
+		query = client.models.generate_content(
+			model="gemini-2.5-flash",contents=messages, 
+			config=types.GenerateContentConfig(
+				tools = [available_functions],
+				system_instruction=system_prompt,
+				temperature = 0))
+		#adding the intial model responses to the list of messages
+		if query.candidates:
+			for candidate in query.candidates:
+				messages.append(candidate.content)
+		#capturing the api usage metadata in a variable
+		usage = query.usage_metadata
+		#printing all of the requested info if verbose flag is present
+		if args.verbose:
+			print(f"User prompt:{args.user_prompt}")
+			print(f"Prompt tokens: {usage.prompt_token_count}")
+			print(f"Response tokens: {usage.candidates_token_count}\n")
+		#error handling for Failed Token API request
+		if query.usage_metadata is None:
+			raise RuntimeError("Failed API Request: Token counts unavailable")
 
+		
+		if not query.function_calls:
+			print("Response:")
+			print(query.text)
+			return
+		else:
+			function_responses = []
+			for call in query.function_calls:
+				function_call_result = call_function(call,args.verbose)
+				if not function_call_result.parts:
+					raise Exception("call_function return object is empty")
+				if not function_call_result.parts[0].function_response:
+					raise Exception("fucntion response list is empty")
+				if not function_call_result.parts[0].function_response.response:
+					raise Exception("functio response attribute is empty ")
+		
+				function_responses.append(function_call_result.parts[0])
+				if args.verbose:
+					print(f"-> {function_call_result.parts[0].function_response.response}")
 
+			messages.append(types.Content(role="user", parts=function_responses))
+	print("Model failed to converge")
+	exit(1)
 if __name__ == "__main__":
     main()
 
